2defaaa0fc3bfbbe592839898feed2153d0d2d10
Frederic Karliczek <f.karliczek@mail.de>
2021-04-05 19:05:53 +0200

    defined actions for all configs

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -7,5 +7,14 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo "Creating symlink for bash profile"
 ln -svf $SCRIPT_DIR/.bash_profile ~
 
-#file=readlink -f ./
-#ln -sv 
+echo "Creating symlink for nvim config"
+nvim_config_dir=~/.config/nvim/
+if [[ ! -d "$nvim_config_dir" ]]; then
+        echo "Config dir not existing. Creating dir"
+        mkdir "$nvim_config_dir"
+fi
+ln -svf "$SCRIPT_DIR/init.vim" "$nvim_config_dir"
+
+echo "Creating symlink for tmux config"
+ln -svf $SCRIPT_DIR/.tmux.conf ~
+
