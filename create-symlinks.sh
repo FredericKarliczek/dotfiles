cacce486d9385864f66e0af56e5a8ead209e89a3
Frederic Karliczek <dev.karliczek@mailbox.org>
2021-05-08 14:19:29 +0200

    Putted configs in own folders

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,7 +5,7 @@ echo "### Starting creating symlinks ###"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 echo "Creating symlink for bash profile"
-ln -svf $SCRIPT_DIR/.bash_profile ~
+ln -svf $SCRIPT_DIR/Bash/.bash_profile ~
 
 echo "Creating symlink for nvim config"
 nvim_config_dir=~/.config/nvim/
@@ -13,8 +13,8 @@ if [[ ! -d "$nvim_config_dir" ]]; then
         echo "Config dir not existing. Creating dir"
         mkdir "$nvim_config_dir"
 fi
-ln -svf "$SCRIPT_DIR/init.vim" "$nvim_config_dir"
+ln -svf "$SCRIPT_DIR/NeoVim/init.vim" "$nvim_config_dir"
 
 echo "Creating symlink for tmux config"
-ln -svf $SCRIPT_DIR/.tmux.conf ~
+ln -svf $SCRIPT_DIR/Tmux/.tmux.conf ~
 
