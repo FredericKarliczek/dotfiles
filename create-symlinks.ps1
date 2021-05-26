e1c0cc380d3014e7d45c3a9d432de1b709671b54
Frederic Karliczek <dev.karliczek@mailbox.org>
2021-05-26 11:59:46 +0200

    Add symlinks for lua files

--- create-symlinks.ps1
+++ create-symlinks.ps1
@@ -5,6 +5,14 @@ $loggedinUser = $loggedinUser.Split('\')
 $loggedinUser = $loggedinUser[1]
 
 ## Nvim
-$nvimDotfile = "C:\Users\$loggedinUser\AppData\local\nvim\init.vim"
-if (Test-Path $nvimDotfile) { Remove-Item $nvimDotfile }
-New-Item -Path $nvimDotfile -ItemType SymbolicLink -Value "$PSScriptRoot\NeoVim\init.vim"
+$nvimLuaDotfile = "C:\Users\$loggedinUser\AppData\local\nvim\init.lua"
+if (Test-Path $nvimLuaDotfile) { Remove-Item $nvimLuaDotfile }
+New-Item -Path $nvimLuaDotfile -ItemType SymbolicLink -Value "$PSScriptRoot\NeoVim\init.lua"
+
+$nvimLuaPath = "C:\Users\$loggedinUser\AppData\local\nvim\lua\"
+if (Test-Path $nvimLuaPath -ErrorAction SilentlyContinue) { Remove-Item $nvimLuaPath -Recurse }
+
+New-Item -Path "$nvimLuaPath\plugins.lua" -ItemType SymbolicLink -Value "$PSScriptRoot\NeoVim\lua\plugins.lua" -Force
+New-Item -Path "$nvimLuaPath\settings.lua" -ItemType SymbolicLink -Value "$PSScriptRoot\NeoVim\lua\settings.lua" -Force
+New-Item -Path "$nvimLuaPath\keybindings.lua" -ItemType SymbolicLink -Value "$PSScriptRoot\NeoVim\lua\keybindings.lua" -Force
+New-Item -Path "$nvimLuaPath\statusline.lua" -ItemType SymbolicLink -Value "$PSScriptRoot\NeoVim\lua\statusline.lua" -Force
