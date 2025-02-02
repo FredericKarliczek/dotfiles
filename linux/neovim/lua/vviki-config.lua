1ee047cd58e6eab7023f23c1899568cbf2b77451
c0der <dev.privacy@mailbox.org>
2025-02-02 12:15:12 +0100

    refactor: Removed old config

--- linux/neovim/lua/vviki-config.lua
+++
@@ -1,8 +0,0 @@
-vim.g.vviki_root = "C:/DATA/Git/private/digital_garden"
--- ### KEYMAPPING 
-local map = vim.api.nvim_set_keymap
-options = { noremap = true }
-
-map('n', '<leader>ww', ':e C:/DATA/Git/private/digital_garden/+Home.adoc<CR>', options)
-
-
